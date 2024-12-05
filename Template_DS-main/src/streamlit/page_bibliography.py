a100b90a5214806b2f3e9757c4f7b4edec500a19
CBESSAIT <166103106+CBESSAIT@users.noreply.github.com>
2024-12-05 12:57:44 +0100

    Update page_bibliography.py

--- Template_DS-main/src/streamlit/page_bibliography.py
+++ Template_DS-main/src/streamlit/page_bibliography.py
@@ -1,5 +1,23 @@
-import streamlit as st
-
 def page_bibliography():
-    st.write("### Bibliographie")
-    st.write("#### A COMPLETER")
+    st.header(":blue[Bibliographie]") 
+    
+    st.write("Les observations **quotidiennes** proviennent de nombreuses stations météorologiques et sont **disponibles** sur : ")
+    st.write("- http://www.bom.gov.au/climate/data")
+
+    st.write("Pour **exemple**, les dernières observations à **Canberra** sont proposées en suivant le lien suivant")
+    st.write("- http://www.bom.gov.au/climate/dwo/IDCJDW2801.latest.shtml")
+    st.write("Les **définitions** sont adaptées de http://www.bom.gov.au/climate/dwo/IDCJDW0000.shtml")
+    st.write("les sources de données utilisées proviennent de :")
+    st.write("- http://www.bom.gov.au/climate/dwo/ ")
+    st.write("- http://www.bom.gov.au/climate/data")
+    st.write("Les **observations météorologiques**  proviennent des stations météorologiques basées dans les **territoires** suivants :")
+    st.write("- Australian Capital Territory avec les stations de Canberra et Tuggeranong")
+    st.write("- New South Wales avec les stations de Sydney, Penrith, Newcastle et Wollongong")
+    st.write("- Victoria avec les stations de Melbourne, Ballarat, Albury-Wodonga et Bendigo")
+    st.write("- Queensland avec les stations de Brisbane, Cairns, Townsville et Gold Coast")
+    st.write("- South Australia avec les stations de Adelaide, Mount Gambier, Renmark et Port Lincoln") 
+    st.write("- Western Australia avec les stations de Perth, Albany, Kalgoorlie-Boulder et Broome") 
+    st.write("- Tasmania avec les stations de Hobart, Launceston, Wynyard et Devonport") 
+    st.write("- Northern Territory avec les stations de Darwin, Alice Springs, Katherine et Tennant Creek") 
+    st.write("- Australia's Antarctic Bases avec les stations de Casey, Davis, Mawson et Macquarie Island") 
+    st.write("- Australia's Offshore Islands avec les stations de Christmas, Cocos, Norfolk et Lord Howe Islands")
