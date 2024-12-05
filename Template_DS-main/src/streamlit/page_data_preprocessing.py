8d7b890f831a7e516cfb0d98dafe7a9f9a31540c
Fabrice S <sergent.fabrice@laposte.net>
2024-12-05 09:58:33 +0100

    Ajout de la description des fonctions

--- Template_DS-main/src/streamlit/page_data_preprocessing.py
+++ Template_DS-main/src/streamlit/page_data_preprocessing.py
@@ -1,37 +1,43 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import plotly.express as px 
+import pandas as pd
+
 
 def page_data_preprocessing(df_hist, df_villes):
-    st.header(":blue[Préprocessing]") 
-    st.write("Le but de cet étape est de préparer le jeu de données pour l'étude. Cette étape passe par : ")
+    st.header(":blue[4. Préprocessing]") 
+    st.write("#### :blue[**4.1. Généralités**]")
+    st.write("Le but de cette étape est de préparer le jeu de données pour l'étude. Cette étape passe par : ")
     st.write(" - une phase d'étude des données;")
     st.write(" - un enrichissement du dataframe à l'aide de données complémentaires;")
     st.write(" - une complétion des NA par des valeurs caculées ou déduites à partir des données existantes.")
-    
-    nlig, ncol = df_hist.shape
-    st.write(f"Le dataframe est composé de {nlig} enregistrements.")
-    
-    st.write("#### :blue[**Etude des valeurs manquantes**]")
-    st.write("Le pourcentage de valeurs manquantes par colonnes est : ") 
-   
- 
-    fig = plt.figure(figsize = (12,8))
-    plt.bar ( df_hist.columns, df_hist.isna().mean() * 100, color = 'lightblue')
-    plt.xticks(rotation=75, ha='right')
-    plt.title ("Poucentage de valeurs nulles par colonne")
-    st.pyplot(fig)
         
     if st.checkbox("Afficher le nombre de NA par colonne") :
-        df = df_hist.isna().mean() * 100
-        st.dataframe(df)
+        fig = px.bar(x = df_hist.columns,y= df_hist.isna().mean()*100)    
+        st.plotly_chart(fig)
         
-    st.write("Quatre variables présentent de nombreuses valeurs manquantes (près de 50% pour SunShine)") 
+    st.write("")
     st.write("Une stratégie de traitement de ces valeurs manquantes doit être définie pour :")   
     st.write(" - Garder un maximum de données;")
     st.write(" - Ne pas modifier substantiellement variables initiales.")
 
-    st.write("#### :blue[**Préprocessing**]")
-    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Date", "Location", "Température", 
+    st.write("#### :blue[**4.2. Création de fonction pour le pré-processing**]")
+    st.write("Afin de mutualiser les traitements, nous avons créer des fonctions de complétion des données : ")
+    st.write("   ***- f_add_info_location*** :  Fonction qui ajoute au dataframe les infos complémentaires sur le lieu : Climat, Longitude, Latitude,  Découpage du continent en 9 zones  ")  
+    st.write("   ***- f_get_median_value*** : Fonction qui retourne la valeur médiane d'une colonne sur l'intervalle [j-3; j+3] ")  
+    st.write("   ***- f_get_mean_value*** : Fonction qui retourne la valeur moyenne d'une colonne sur l'intervalle [j-3; j+3] ")     
+    st.write("   ***- f_create_df_mean*** : création d'un dataframe contenant les valeurs moyennes pour une journée donnée sur un climat donné ")  
+    st.write("   ***- f_maj_na_mean*** : Mise à jour des valeurs Na d'une liste de colonne à l'aide de la moyenne")  
+    st.write("   ***- f_maj_na_mean*** : Mise à jour des valeurs Na d'une liste de colonne à l'aide de la médiane")  
+    st.write("   ***- f_maj_humidity*** : Mise à jour des données manquantes sur le colonne Humidity. Si Raintoday = 1 , Alors Humidity = 100,   sinon  par la moyenne des humidité de la semaine pour le même clinat ")  
+    st.write("   ***- f_maj_rainfall*** : Mise à jour des données manquantes sur le colonne Rainfall. Si Raintoday = 0 , Alors Rainfall = 0,  sinon  par la moyenne des rainfall de la semaine pour le même clinat ")  
+    st.write("   ***- f_maj_windspeed*** : Description : Mise à jour des valeurs manquantes de la colonne WindSpeedGust par le max de WindSpeed9am et WindSpeed3pm")
+    st.write("   ***- f_maj_windgustdir*** : Mise à jour des valeurs manquantes de la colonne WindGustDir par la valeur de WindGustDir3pm")
+ 
+    
+    st.write("#### :blue[**4.3. Pré-processing par colonne**]")   
+    
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Date", "Lieu", "Température", 
                                                         "Pression", "Précipitation", "Vent", "Données complémentaires"])
 
     with tab1: # Données Date 
@@ -50,16 +56,7 @@ def page_data_preprocessing(df_hist, df_villes):
     
         if st.checkbox("Afficher le dataframe utilisé pour enrichir les données localisation") :
             st.dataframe(df_villes.head(10))
-        
-        st.write("**Préprocessing des données : ")
-        st.write(" - **Climat :** A chaque ville, nous associerons le code climat suivant ") 
-        st.write("                0 : Aride , Désert")
-        st.write("                1 : Subtropical")
-        st.write("                2 : Tropical")
-        st.write("                3 : Tempéré") 
-    
-        st.write(" - **Coordonnées :** A chaque ville, nous associerons la **Longitude** et **Latitude** ") 
-    
+         
    
     with tab3:  # Température
         st.write("**- Temp9am  :** Remplacement des NA par la moyenne de Temp9am sur le même climat sur +/- 3 jours.")
@@ -97,3 +94,10 @@ def page_data_preprocessing(df_hist, df_villes):
         st.write("**- Sunshine :** Suppression de la colonne.")
         st.write("**- Cloud9am :** Suppression de la colonne.")
         st.write("**- Cloud3pm :** Suppression de la colonne.")
+        
+
+    st.write("#### :blue[**4.4. Résultat du pré-processing**]")   
+    if st.checkbox("Afficher le nombre de NA par colonne après pré-processing") :
+        df_result = pd.read_csv("./../../Data/dataset.csv")
+        fig = px.bar(x = df_result.columns,y= df_result.isna().mean()*100)    
+        st.plotly_chart(fig)
